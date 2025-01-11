c8755c8503abf73e037cb461172a7d1fe3d979bf
Luiz Silva. <160427605+luizOASSilva@users.noreply.github.com>
2025-01-11 16:10:46 -0300

    criando um repositório

---
+++ JS/Aula 1 - Console.log/script.js
@@ -0,0 +1,12 @@
+console.log('"Hello" world');
+console.log("'Hello' world");
+console.log(`"Hello" 'world'`);
+console.log(123.123, 12)
+
+/* Exercício 
+
+Escreva "Meu nome é "NOME". Estou aprendendo JavaScript as 15 da tarde" dentro do comando console.log
+
+*/
+
+console.log('Meu nome é Luiz. Estou aprendendo JavaScript às', 15, 'da tarde');
---
+++ JS/Aula 2 - Comentários em JS/script.js
@@ -0,0 +1,9 @@
+// -> comentário de linha (funciona apenas em uma linha)    |    /* comentário */ -> Comentário de bloco (se expande para várias linhas)
+
+// comentários são ignorados pela engine do JavaScript ao executar o código
+console.log('Hello world'); 
+// esse comentário também é ignorado, apenas visível no editor de código
+console.log('Comentário anterior ignorado');
+/* comentário de bloco
+para várias linhas
+*/
