dc3562077160817d3ee6a9733b979669c253c102
Luiz Silva. <160427605+luizOASSilva@users.noreply.github.com>
2025-01-19 23:51:06 -0300

    exercise

--- JS/Aula 22 - Exercício com array, função e objeto/assets/JS/script.js
+++ JS/Aula 22 - Exercício com array, função e objeto/assets/JS/script.js
@@ -1,8 +1,21 @@
 function meuEscopo () {
     const form = document.querySelector('#form');
     form.onsubmit = function(evt) {
+        const pessoas = [];
+
+        function recebeData() {
+            console.log('passou aqui');
+        }
+
         evt.preventDefault();
+        const nome = form.querySelector('#nome');
+        const sobrenome = form.querySelector('#sobrenome');
+        const altura = form.querySelector('#altura');
+
+        form.addEventListener('submit', )
     }
+
+    
 }
 meuEscopo();
 
