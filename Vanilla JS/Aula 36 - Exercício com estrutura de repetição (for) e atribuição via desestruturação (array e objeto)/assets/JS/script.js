ea0977b26ef9f4498342f0019dcf980bb0e7d3c3
Luiz Silva. <160427605+luizOASSilva@users.noreply.github.com>
2025-01-24 01:28:35 -0300

    exercise

---
+++ Vanilla JS/Aula 36 - Exercício com estrutura de repetição (for) e atribuição via desestruturação (array e objeto)/assets/JS/script.js
@@ -0,0 +1,40 @@
+// utilize o array "elementos" para apresentar texto na tela. Utilizando todos os conceitos aprendidos até hoje.
+
+const container = document.querySelector('.container');
+const div = criaDiv();
+container.appendChild(div);
+
+const elementos = [
+    {tagName: 'h1', texto: 'Frase 1'},
+    {tagName: 'div', texto: 'Frase 2'},
+    {tagName: 'section', texto: 'Frase 3'},
+    {tagName: 'footer', texto: 'Frase 4'},
+];
+
+for (let i = 0; i < elementos.length; i++) {
+    obj = elementos[i];
+    ({tagName, texto} = obj);
+
+    console.log(tagName, texto);
+
+    const elemento = criaElemento(tagName);
+
+    setText(elemento, texto);
+}
+
+function criaElemento (tagName) {
+    const elemento = document.createElement(tagName); 
+    return elemento;
+}
+
+function setText (elemento, texto) {
+    // elemento.innerHTML = texto;
+    textoNode = document.createTextNode(texto);
+    elemento.appendChild(textoNode);
+    div.appendChild(elemento);
+}
+
+function criaDiv () {
+    const div = document.createElement('div');
+    return div;
+}
