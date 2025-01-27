b824bd5f3e51debf09bc47b7611d49bc02afa9b0
Luiz Silva. <160427605+luizOASSilva@users.noreply.github.com>
2025-01-27 03:10:04 -0300

    timer exercise

---
+++ Vanilla JS/Aula 47 - Exercício contruindo um timer/assets/JS/script.js
@@ -0,0 +1,41 @@
+const form = document.querySelector('#form');
+const h1 = document.querySelector('h1');
+let buttonPlay = document.querySelector('#play');
+let playing;
+
+let UnixDate = new Date(0 + (60 * 60 * 3 * 1000)) // resetando objeto Date para resetar a data
+
+form.addEventListener('submit', function timerAction (evt) {
+    evt.preventDefault();
+
+    if(evt.submitter.id === 'play'){
+        playTimer();
+        buttonPlay.setAttribute('disabled', true);
+    } 
+    if(evt.submitter.id === 'pause'){
+        pauseTimer();
+        buttonPlay.removeAttribute('disabled');
+    }
+    if(evt.submitter.id === 'reset'){
+        resetTimer();
+        buttonPlay.removeAttribute('disabled');
+    }
+});
+
+function playTimer() {
+    clearInterval(playing); 
+    playing = setInterval(() => {
+        UnixDate.setSeconds(UnixDate.getSeconds() + 1);
+        h1.innerHTML = UnixDate.toLocaleTimeString('pt-BR');  
+    }, 1000)
+}
+
+function pauseTimer() {
+    clearInterval(playing); 
+}
+
+function resetTimer() {
+    UnixDate = new Date(0 + (60 * 60 * 3 * 1000)) // resetando objeto Date para resetar a data
+    h1.innerHTML = '00:00:00';   
+    clearInterval(playing); 
+}
