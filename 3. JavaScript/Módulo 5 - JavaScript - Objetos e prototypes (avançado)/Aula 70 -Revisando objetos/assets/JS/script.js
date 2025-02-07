1f716b23fb40faa227f3a054201b14e6e6029e6e
Luiz Silva. <160427605+luizOASSilva@users.noreply.github.com>
2025-02-07 19:20:59 -0300

    object

--- 3. JavaScript/Módulo 5 - JavaScript - Objetos e prototypes (avançado)/Aula 70 -Revisando objetos/assets/JS/script.js
+++ 3. JavaScript/Módulo 5 - JavaScript - Objetos e prototypes (avançado)/Aula 70 -Revisando objetos/assets/JS/script.js
@@ -1,18 +1,103 @@
-const a1 = [10, 20, 30, 40];
-let total = 0;
-let total2 = 0;
+// objetos podem ser literais
+const pessoa = {
+    nome: 'Luiz', // propriedade do objeto
+    sobrenome: 'Otávio' 
+}
+console.log(pessoa.nome, pessoa['sobrenome']); // as propriedades do objeto podem ser acessadas por notação de ponto ou de colchetes. Em caso da notação de colchetes, o nome da propriedade deve vir entre aspas dentro do colchete
+// **obs: objetos podem ser conhecidos muitas vezes também como arrays associativos
 
-a1.forEach((valor, indice, array) => { // forEach executa uma função para cada elemento do array
-    // console.log(valor); valor: valor que foreach aponta naquela iteração
-    // console.log(indice); indice: posição do elemento apontado no array. Começando por 0
-    // console.log(array); array: array completo
-    total += valor; // soma cada valor recebido por parâmetro à variável global "valor"
-});
+// no caso da notação de colchetes, podemos utilizar valores dinâmicos
 
-console.log(total);
+const animal = {
+    nome: 'Berenice',
+    tipoAnimal: 'Gato'
+};
 
-// forma reduzida, utilizando arrow function
+const raca = 'tipoAnimal'; // nesse caso raca recebe uma string
+console.log(animal[raca]); // acessa raca cujo valor é "tipoAnimal" no objeto animal
 
-a1.forEach(valor => total2 -= valor); // diminui cada valor recebido por parâmetro à variável global "valor"
+const pessoa2 = new Object(); // podemos criar o objeto por meio do seu construtor também
+pessoa2.idade = 19; // e definir proprieades à eles por meio de notação de pontos
+pessoa2.estaTrabalhando = false;
+console.log(pessoa2); // saída = { idade: 19, estaTrabalhando: false }
 
-console.log(total2);
+delete pessoa2.estaTrabalhando; // com "delete", podemos deletar uma propriedade do valor. Nesse caso deletamos a propriedade "estaTrabalhando"
+console.log(pessoa2); // saída = { idade: 19 }
+
+const op = {
+    num1: 10,
+    num2: 10,
+    sum: function () {
+        console.log(this.num1 + this.num2);
+    },
+    sub: function () {
+        console.log(this.num1 - this.num2);
+    }
+}
+
+// funções dentro de objetos são chamadas de métodos. A vantagem é o livre acesso às propriedades definidas no objeto
+
+const teste = op; // atribuindo o objeto "op" à variável "teste"
+
+teste.sum(); // sum() é um método do ob op1
+
+teste.mult = function() { // podemos atribuir métodos diretamente por notação de ponto
+    console.log(this.num1 * this.num2); // 10 * 10 = 100
+};
+
+teste.mult(); // acessa o método mult() do objeto op1
+
+// exemplo mais real
+
+const newPeople = {
+    namePeople: 'Luiz',
+    age: 19,
+    getBirthDate: function() {
+        const currentDate = new Date(); // pega a data atual e atribui à variável currentDate
+        const year = currentDate.getFullYear(); // pega o ano atual da data em "currentDate"
+        return year - this.age; // faz o ano atual - a idade atual
+    }
+};
+
+console.log(newPeople.getBirthDate()); // acessando o método do objeto "newPeople"
+
+for(let propriedade in newPeople) { // variável "propriedade" passa por todas as propriedades do objeto
+    console.log(propriedade);
+}
+
+// factory functions
+
+function criapessoa(nome, sobrenome) { // factory functions retornam um objeto
+    return {
+        nome,
+        sobrenome,
+        get nomeCompleto() { // métodos com get se transformam em uma propriedade do objeto
+            return `${this.nome} ${this.sobrenome}`;
+        }
+    };
+}
+
+const pessoa1 = criapessoa('Luiz', 'Otávio');
+console.log(pessoa1.nomeCompleto);
+
+// constructor function 
+
+function Pessoa2(nome, sobrenome) {
+    this.nome = nome; // this é individual a cada objeto, atrela this a esse escopo do próprio objeto e o retorna de forma implícita
+    this.sobrenome = sobrenome;
+}
+
+const pessoa3 = new Pessoa2('Helena', 'Daher');
+// new cria um objeto vazio
+// new atrela a referência this ao objeto
+
+
+console.log(pessoa3); // a saída deixa explicito o construtor. Nesse caso "Pessoa"
+
+Object.freeze(pessoa3); // freeze() impossiblita o objeto de ser modificado
+
+function Pessoa3(nome, sobrenome) {
+    this.nome = nome; // this é individual a cada objeto, atrela this a esse escopo do próprio objeto e o retorna de forma implícita
+    this.sobrenome = sobrenome;
+    Object.freeze(this); // podemos usar freeze() para travar this. Desta forma os valores do objeto não poderão ser modificados, nem criados e nem deletados
+}
