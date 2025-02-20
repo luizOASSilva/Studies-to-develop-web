f490d28eb6a57160ec8d8f690bed612ac25d532e
Luiz Silva. <160427605+luizOASSilva@users.noreply.github.com>
2025-02-19 23:57:54 -0300

    Map() Object

--- 3. JavaScript/Módulo 5 - JavaScript - Objetos e prototypes (avançado)/Aula 80 - Objeto Map()/assets/JS/script.js
+++ 3. JavaScript/Módulo 5 - JavaScript - Objetos e prototypes (avançado)/Aula 80 - Objeto Map()/assets/JS/script.js
@@ -1,3 +1,4 @@
+// Map() contém pares chave-valor e lembra a ordem de inserção original das chaves.
 
 const pessoas = [
     { id: 3, nome: 'Luiz' },
@@ -5,10 +6,26 @@ const pessoas = [
     { id: 1, nome: 'Milena' },
 ];
 
-const novasPessoas = {};
+const novasPessoas = new Map(); // cria um objeto "Map()" que irá lembrar a ordem das chaves no momento de sua inserção
 
-for(const pessoa of pessoas) { 
-    const {id, nome} = pessoa
-    console.log(id);
-    console.log(nome);
+for(const {id, nome} of pessoas) {
+    novasPessoas.set(id, nome); // "Map.set()" adiciona chaves com valores ao objeto
 }
+
+console.log(novasPessoas);
+
+const map1 = new Map(); // instanciando "Map()"
+
+map1.set('a', 1);
+map1.set('b', 2);
+map1.set('c', 3);
+
+console.log(map1);
+
+console.log(map1.get('a')); // "Map.get()" retorna o valor atribuído à chave procurada
+
+console.log(map1.size); // "Map.size" retorna o tamanho do objeto de acordo com a quantidade de chaves
+
+map1.delete('b'); // "Map.delete()" deleta uma chave
+
+console.log(map1.size);
