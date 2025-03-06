71e97e2cb7b61779a76c82bf4dbcae692a8c5557
Luiz Silva. <160427605+luizOASSilva@users.noreply.github.com>
2025-03-05 23:39:07 -0300

    config webpack

--- 3. JavaScript/Módulo 8 - JavaScript Tooling e ES6 Modules/Aula 98 - Core-js e generator-runtime/webpack.config.js
+++ 3. JavaScript/Módulo 8 - JavaScript Tooling e ES6 Modules/Aula 98 - Core-js e generator-runtime/webpack.config.js
@@ -1,9 +1,29 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/main.js',
+    mode: 'production',
+    entry: './src/index.js',
     output: {
+        path: path.resolve(__dirname, 'public', 'assets', 'JS'),
         filename: 'bundle.js',
-        path: path.resolve(__dirname, '')
-    }
-}
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    },
+                },
+            },
+            {
+                test: /\.css$/i, 
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+    devtool: 'source-map',
+};
