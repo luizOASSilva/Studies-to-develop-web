c8795c430f63048459a8ec3f8a9327534b5318e7
Luiz Silva. <160427605+luizOASSilva@users.noreply.github.com>
2025-03-07 23:28:47 -0300

    webapack config

--- 3. JavaScript/Módulo 8 - JavaScript Tooling e ES6 Modules/Aula 99 - Criando um gerador de senhas/webpack.config.js
+++ 3. JavaScript/Módulo 8 - JavaScript Tooling e ES6 Modules/Aula 99 - Criando um gerador de senhas/webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
             }, 
             {
                 test: /\.css$/i,
-                use: 'css-loader'
+                use: ['style-loader", "css-loader'],
             }
         ]
     },
