1af620354da2ca4d3aaaab3930889bb35742a338
Luiz Silva. <160427605+luizOASSilva@users.noreply.github.com>
2025-03-08 00:16:45 -0300

    more studies

--- 3. JavaScript/Módulo 8 - JavaScript Tooling e ES6 Modules/Aula 98 - Core-js e generator-runtime/src/index.js
+++ 3. JavaScript/Módulo 8 - JavaScript Tooling e ES6 Modules/Aula 98 - Core-js e generator-runtime/src/index.js
@@ -1,1 +1,14 @@
-import './assets/CSS/index.css'
+// bundles criados pelo babel por meio do transpilador, não suportam javascript assíncrono
+
+// para isso, importa-se "core-js" e "regenerator-runtime" dos módulos do node_modules
+
+import 'core-js/stable/';
+import 'regenerator-runtime/runtime'; 
+
+// resolvendo o problema de compatibilidade de JavaScript assíncrono com navegadores antigos com os imports
+
+import './assets/CSS/index.css' 
+
+import execute from './modules/promises';
+
+execute(); // executando a promise
