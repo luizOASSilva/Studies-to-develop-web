9a39a3602a218335f297545a842f9fbb26596d4b
Luiz Silva. <160427605+luizOASSilva@users.noreply.github.com>
2025-03-07 22:35:21 -0300

    Update webpack.config.js

--- 3. JavaScript/Módulo 8 - JavaScript Tooling e ES6 Modules/Aula 99 - Criando um gerador de senhas/webpack.config.js
+++ 3. JavaScript/Módulo 8 - JavaScript Tooling e ES6 Modules/Aula 99 - Criando um gerador de senhas/webpack.config.js
@@ -1,1 +1,23 @@
 const path = require('path');
+
+module.exports = {
+    entry: './src/JS/main.js',
+    output: {
+        filename: '',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    option: {
+                        presets: ['@babel/preset-env'],
+                    }   
+                }
+            }
+        ]
+    }
+};
