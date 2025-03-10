44138a955840787a34cf783e6894aeecfc1d6d1e
Luiz Silva. <160427605+luizOASSilva@users.noreply.github.com>
2025-03-10 18:31:31 -0300

    change name

--- 3. JavaScript/Módulo 9 - JavaScript - Node.js, npm, Express.js e mongoDB/Aula 102 - Listando arquivos com F5 e recursão mútua/index.js
+++ 3. JavaScript/Módulo 9 - JavaScript - Node.js, npm, Express.js e mongoDB/Aula 102 - Listando arquivos com F5 e recursão mútua/index.js
@@ -1,8 +1,8 @@
-const fs = require('fs').promises; // importando o módulo "fs" já existente em node_modules. Este mnódulo permite e interação com o sistema de arquivos de uma forma modelada
-const path = require('path'); // importando o módulo já existente em node_modules
+// const fs = require('fs').promises; // importando o módulo "fs" já existente em node_modules. Este mnódulo permite e interação com o sistema de arquivos de uma forma modelada
+// const path = require('path'); // importando o módulo já existente em node_modules
 
-fs.readdir('')
-    .then(files => console.log(files))
-    .catch(e => console.log(e))
+// fs.readdir('')
+//     .then(files => console.log(files))
+//     .catch(e => console.log(e))
 
 
