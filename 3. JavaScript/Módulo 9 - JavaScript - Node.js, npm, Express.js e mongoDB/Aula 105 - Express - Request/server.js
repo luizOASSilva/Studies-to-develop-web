73f30b57d9601ba10f0029cbb8e0ab165cfce045
Luiz Silva. <160427605+luizOASSilva@users.noreply.github.com>
2025-03-11 21:26:32 -0300

    express

--- 3. JavaScript/Módulo 9 - JavaScript - Node.js, npm, Express.js e mongoDB/Aula 105 - Express - Request/server.js
+++ 3. JavaScript/Módulo 9 - JavaScript - Node.js, npm, Express.js e mongoDB/Aula 105 - Express - Request/server.js
@@ -6,5 +6,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(3000, () => {
-    console.log('server run in: https://localhost:3000')
+    console.log('server run in: https://localhost:3000');
 });
