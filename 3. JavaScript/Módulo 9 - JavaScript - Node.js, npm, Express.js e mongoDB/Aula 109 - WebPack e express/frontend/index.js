e95eb26d2885fb961977ec7cd1470f0db1968daa
Luiz Silva. <160427605+luizOASSilva@users.noreply.github.com>
2025-03-13 18:41:35 -0300

    express + webpack

--- 3. JavaScript/Módulo 9 - JavaScript - Node.js, npm, Express.js e mongoDB/Aula 109 - WebPack e express/frontend/index.js
+++ 3. JavaScript/Módulo 9 - JavaScript - Node.js, npm, Express.js e mongoDB/Aula 109 - WebPack e express/frontend/index.js
@@ -1,13 +1,2 @@
 import './assets/CSS/style.css' 
 
-const obj = {
-    nome: 'Luiz',
-    idade: 19
-};
-
-const newObj = { ...obj };
-
-console.log(newObj);
-
-const h1 = document.querySelector('h1');
-h1.innerHTML = 'escrevido';
